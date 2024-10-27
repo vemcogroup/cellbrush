a41349d1c4180b87a496af1e22cddc5bee396eb2
Andreas Hennings <andreas@dqxtech.net>
2024-10-27 17:02:06 +0100

    Issue #14: Finish the TableSectionTest::testRegularTable() test.

--- tests/src/TableSectionTest.php
+++ tests/src/TableSectionTest.php
@@ -3,6 +3,7 @@
 namespace Donquixote\Cellbrush\Tests;
 
 use Donquixote\Cellbrush\Axis\DynamicAxis;
+use Donquixote\Cellbrush\Html\Multiple\StaticAttributesMap;
 use Donquixote\Cellbrush\TSection\TableSection;
 
 class TableSectionTest extends \PHPUnit_Framework_TestCase {
@@ -10,7 +11,7 @@ class TableSectionTest extends \PHPUnit_Framework_TestCase {
   function testRegularTable() {
     $columns = new DynamicAxis();
     $columns->addNames(['c0', 'c1', 'c2']);
-    $tsection = new TableSection($columns);
+    $tsection = new TableSection('tbody');
     $tsection->addRow('r0')
       ->td('c0', '00')
       ->td('c1', '01')
@@ -21,7 +22,17 @@ class TableSectionTest extends \PHPUnit_Framework_TestCase {
       ->td('c1', '11')
       ->td('c2', '12')
     ;
-    # print_r($tsection->getRawMatrix($columns->takeSnapshot()));
+    $tableColAttributes = StaticAttributesMap::create([], []);
+    $output = $tsection->render($columns->takeSnapshot(), $tableColAttributes);
+    $this->assertSame(
+      <<<'EOT'
+  <tbody>
+    <tr><td>00</td><td>01</td><td>02</td></tr>
+    <tr><td>10</td><td>11</td><td>12</td></tr>
+  </tbody>
+
+EOT,
+      $output);
   }
 
 }
